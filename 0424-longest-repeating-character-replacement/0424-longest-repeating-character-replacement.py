dfa9e29dfc6e66430b08a90de260728b1f90e934
anjani-09 <55249418+anjani-09@users.noreply.github.com>
2023-12-07 18:48:03 -0700

    Time: 89 ms (81.31%), Space: 16.4 MB (22.14%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,9 +1,8 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         lookup = {}
-        maxf = 0
-        l = 0
         res = 0
+        l = 0
         for r in range(len(s)):
             lookup[s[r]] = 1 + lookup.get(s[r],0)
             maxf = max(lookup.values())
@@ -13,4 +12,4 @@ class Solution:
             else:
                 res = max(res, r - l + 1)
         return res
-        
+         
