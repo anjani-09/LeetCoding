28080ca4f0685ebbe0e16c9f1f8cf1f0e804a1ed
anjani-09 <55249418+anjani-09@users.noreply.github.com>
2024-01-28 17:27:53 -0700

    Time: 53 ms (89.01%), Space: 16.8 MB (61.43%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,28 +1,28 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
+       
         if len(s1) > len(s2):
             return False
-        helper = dict()
+        helper = {}
+        
         for i in range(97, 123):
             helper[i] = 0
-        
-        temp1 = helper.copy()
+        helper1 = helper.copy()
+        helper2 = helper.copy()
         
         for i in range(len(s1)):
-            temp1[ord(s1[i])]+=1
+            helper1[ord(s1[i])]+=1
+        for i in range(len(s1)-1):
+            helper2[ord(s2[i])]+=1
         
-        temp2 = helper.copy()
-        
-        for j in range(len(s1)-1):
-            temp2[ord(s2[j])]+=1
-            
         l,r = 0, len(s1)-1
         while r < len(s2):
-            temp2[ord(s2[r])]+=1
-            if temp1 == temp2:
+            helper2[ord(s2[r])] += 1
+            if helper1 == helper2:
                 return True
-            temp2[ord(s2[l])]-=1
+            helper2[ord(s2[l])]-=1
             l+=1
             r+=1
         return False
+        
         
