07619544babdea4f2e13a18baf0ab6ccf2f36ddf
anjani-09 <55249418+anjani-09@users.noreply.github.com>
2024-01-28 18:17:01 -0700

    Time: 742 ms (54.45%), Space: 36.3 MB (88.70%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -1,18 +1,15 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        helper = []
-        for p,s in zip(position, speed):
-            helper.append([p,s])
+        helper = [[p,s] for p,s in zip(position, speed)]
         helper.sort(reverse = True)
+        
         stack = []
+        
         for p,s in helper:
-            time = (target - p)/s
-            stack.append(time)
-            if len(stack) > 1 and (stack[-1] <= stack[-2]):
-                stack.pop()
+            stack.append((target - p)/s)
+            
+            if len(stack) >= 2:
+                if stack[-1] <= stack[-2]:
+                    stack.pop()
         return len(stack)
-            
-            
-            
-            
         
